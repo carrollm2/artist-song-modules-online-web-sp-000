af7a3daeb3f5bab8824c734123070474120fda4b
Michael Carroll <carrollm2@gmail.com>
2019-11-12 02:37:53 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,9 +2,20 @@ require 'pry'
 
 module Findable
   module ClassMethods
+    # def find_by_name(name)
+    #   objects = []
+    #   ObjectSpace.each_object(self){|obj| objects << obj}
+    #   objects.detect{|a| a.name == name}
+    # end
     def find_by_name(name)
+      objects_names = []
       objects = []
-      ObjectSpace.each_object(self){|obj| objects << obj}
+      ObjectSpace.each_object(self) do |obj|
+        if objects_names.include?(obj.name) == false
+          objects_names << obj.name
+          objects << obj
+        end
+      end
       objects.detect{|a| a.name == name}
     end
   end
